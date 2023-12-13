b9f96a73f924153d9d8c0175ea56364d9b07c95e
stygianlgdonic <stygianlgdonic@gmail.com>
2023-12-13 13:07:54 +0500

    url update

--- client/src/containers/ProgressScreen/index.tsx
+++ client/src/containers/ProgressScreen/index.tsx
@@ -52,7 +52,7 @@ const ProgressScreen: React.FC<Props> = ({ filteredCsvData, uploadedVideoLink })
         }
         const url = urls[0].url;
         try {
-            const response = await axios.get(`http://51.20.87.7:3001/capture?url=${url}&videoLink=${uploadedVideoLink}`); 
+            const response = await axios.get(`http://webranksolutions.eu/api/capture?url=${url}&videoLink=${uploadedVideoLink}`); 
             if (response.status === 200) {  
                 setVideoLink((prevVideoLink: any) => {
                     const newArray = [...prevVideoLink];
